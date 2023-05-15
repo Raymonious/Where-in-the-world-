94597fa12d2071630e940a36e172e95d8e1640e9
Raymond Chen <rayche@kth.se>
2023-05-15 14:32:05 +0200

    added some styles to favPop

--- src/view/favPopupView.jsx
+++ src/view/favPopupView.jsx
@@ -5,30 +5,31 @@ import  DialogTitle  from "@mui/material/DialogTitle";
 import DialogContent  from "@mui/material/DialogContent";
 import  DialogContentText  from "@mui/material/DialogContentText";
 import  DialogActions  from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 export default function FavPopupView(props){
-  function handleGame(){
+  function handleGameACB(){
     window.location.hash = "#/game";
   }
-  function handleHome(){
+  function handleHomeACB(){
     window.location.hash = "#/home";
   }
 
 
 return (<Dialog open={true}>
-        <DialogTitle>
+        <DialogTitle sx = {{fontFamily: 'Audiowide'}}>
           {"Your favorite list is empty and needs a little TLC"}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText>
+          <DialogContentText sx = {{fontFamily: 'Audiowide'}}s>
             Switch to Game mode to start building up your list of favorite countries!
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleHome} >Home</Button>
-          <Button onClick = {handleGame} autoFocus = {true}>
-            Game
-          </Button>
+        <IconButton title="Game!" onClick = {handleGameACB}><SportsEsportsOutlinedIcon sx={{fontSize: '1.5em', color: 'black'}}></SportsEsportsOutlinedIcon></IconButton>
+        <IconButton title="Back to Home" onClick= {handleHomeACB} ><HomeOutlinedIcon sx={{fontSize: '1.5em',color: 'black'}}></HomeOutlinedIcon></IconButton>
         </DialogActions>
       </Dialog>);
 
