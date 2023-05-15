01e4b8ed501dada93d2ec4305211af048015e217
Omid Mircoram <mircoram@kth.se>
2023-05-15 13:16:57 +0200

    reload efter log out

--- src/presenter/logoutPresenter.jsx
+++ src/presenter/logoutPresenter.jsx
@@ -16,6 +16,7 @@ export default function Logout(){
     else{
         function handleLogOut(){
             signOut(fireBaseAuth);
+            window.location.reload()
          }   
         return (
             <LogoutView onLogout = {handleLogOut}/>
