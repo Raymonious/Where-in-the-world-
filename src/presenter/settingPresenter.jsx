d2361e8bcabea867b8ecc7ec53fb39e9fb677c47
Omid Mircoram <mircoram@kth.se>
2023-05-15 14:50:09 +0200

    changing from settings to profile page

--- src/presenter/settingPresenter.jsx
+++ src/presenter/settingPresenter.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import SettingView from '../view/SettingView';
 import { useRecoilState } from 'recoil';
-import { isGrantedAccess } from '../model/atoms';
+import {isGrantedAccess, playerLatestStreak} from '../model/atoms';
 import { Navigate } from 'react-router-dom';
 import SuspenseView from '../view/suspenseView';
+import {playerHighScore} from "../model/persistant_atoms.js";
 export default function Login(){
     const[access] = useRecoilState(isGrantedAccess);
+    const [userLatestStreak] = useRecoilState(playerLatestStreak)
     if(!access) {
         if(access === null) return <SuspenseView/>
         return <Navigate to="/login" replace/>
     }
     else{
+        const [userHighStreak] = useRecoilState(playerHighScore)
+
         return (
-            <SettingView/>
+            <SettingView highStreak = {userHighStreak} recentStreak = {userLatestStreak}/>
         );
     }
 }
--- src/view/SettingView.jsx
+++ src/view/SettingView.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import photo from '../assets/photo.png'
-export default function LoginView() {
+import {fireBaseAuth} from "../model/persistant_atoms.js";
+export default function LoginView(props) {
     return (
-        <div width="100%" className="login">
+        <div className="login">
             <div className="formBox setting">
-                <img src={photo} alt="" className='photo'/>
                 <form className='from'>
                     <h5>Account Details</h5>
                     <div className='formItem'>
-                        <label>First Name</label>
-                        <input type="text" className="ipt" name='email' />
+                        <label>Display name</label>
+                        <span>{fireBaseAuth.currentUser.displayName}</span>
                     </div>
                     <div className='formItem'>
-                        <label>Last Name</label>
-                        <input type="text" className="ipt" name='email' />
+                        <label>Highest streak</label>
+                        <span>{props.highStreak}</span>
                     </div>
                     <div className='formItem'>
-                        <label>Email</label>
-                        <input type="email" className="ipt" name='email' />
+                        <label>Most recent streak</label>
+                        <span>{props.recentStreak}</span>
                     </div>
-                    <div className='formItem'>
-                        <label>Account Type</label>
-                        <input type="text" className="ipt" name='email' />
-                    </div>
-                    <button className='btn' type='submit'>SAVE NEW CHANGES</button>
                 </form>
             </div>
         </div>
